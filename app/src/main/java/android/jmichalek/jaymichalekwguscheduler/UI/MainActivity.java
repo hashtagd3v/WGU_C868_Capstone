d90651f21e08747b7979fb825c3ac2ea5abb6cc3
Jay M <jaycoinminer@gmail.com>
2021-09-29 22:42:40 -0500

    Home Screen Design plus first intent: Enter Button

--- app/src/main/java/android/jmichalek/jaymichalekwguscheduler/UI/MainActivity.java
+++ app/src/main/java/android/jmichalek/jaymichalekwguscheduler/UI/MainActivity.java
@@ -2,8 +2,10 @@ package android.jmichalek.jaymichalekwguscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.jmichalek.jaymichalekwguscheduler.R;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** When Enter button is pressed, this method brings user to next screen which
+     * is TermList screen.*/
+    public void enterButton(View view) {
+
+        Intent intent = new Intent(MainActivity.this, TermList.class);
+        startActivity(intent);
+
+    }
+
 }
---
+++ app/src/main/java/android/jmichalek/jaymichalekwguscheduler/UI/TermList.java
@@ -0,0 +1,15 @@
+package android.jmichalek.jaymichalekwguscheduler.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.jmichalek.jaymichalekwguscheduler.R;
+import android.os.Bundle;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
