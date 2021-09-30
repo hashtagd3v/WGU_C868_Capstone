13b1e132c5d02ada8825347321cdb9760b765d77
Jay M <jaycoinminer@gmail.com>
2021-09-29 23:20:25 -0500

    Back Button and retain previous information

--- app/src/main/java/android/jmichalek/jaymichalekwguscheduler/UI/TermList.java
+++ app/src/main/java/android/jmichalek/jaymichalekwguscheduler/UI/TermList.java
@@ -2,14 +2,34 @@ package android.jmichalek.jaymichalekwguscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.jmichalek.jaymichalekwguscheduler.R;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class TermList extends AppCompatActivity {
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+
+        // Allows user to switch back to previous screen & retain information.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    /** This method enables user to switch back to previous screen.*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
