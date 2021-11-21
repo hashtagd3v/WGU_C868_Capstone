20aa8234279a330074abba0c266ff93a13ad640d
Jay M <jaycoinminer@gmail.com>
2021-11-20 22:30:06 -0600

    Deleted PA and OA tables.

--- app/src/main/java/android/jmichalek/jaymichalek_capstone/All/Entities/ObjectiveAssessment.java
+++ app/src/main/java/android/jmichalek/jaymichalek_capstone/All/Entities/ObjectiveAssessment.java
@@ -1,23 +1,8 @@
 package android.jmichalek.jaymichalek_capstone.All.Entities;
 
-import static androidx.room.ForeignKey.CASCADE;
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.ForeignKey;
-
-@Entity(tableName = "objective_assessment", foreignKeys = {
-        @ForeignKey(
-                entity = Assessment.class,
-                parentColumns = "assessment_id",
-                childColumns = "objective_reference",
-                onUpdate = CASCADE,
-                onDelete = CASCADE
-        )
-})
 public class ObjectiveAssessment extends Assessment{
 
-    @ColumnInfo(name = "objective_reference")
     private int objective_reference;
 
     public ObjectiveAssessment(int assessment_id, String assessmentName, String assessmentStart, String assessmentEnd, int courseID, String type, int objective_reference) {
--- app/src/main/java/android/jmichalek/jaymichalek_capstone/All/Entities/PerformanceAssessment.java
+++ app/src/main/java/android/jmichalek/jaymichalek_capstone/All/Entities/PerformanceAssessment.java
@@ -1,23 +1,7 @@
 package android.jmichalek.jaymichalek_capstone.All.Entities;
 
-import static androidx.room.ForeignKey.CASCADE;
-
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.ForeignKey;
-
-@Entity(tableName = "performance_assessment", foreignKeys = {
-        @ForeignKey(
-                entity = Assessment.class,
-                parentColumns = "assessment_id",
-                childColumns = "assessment_reference",
-                onUpdate = CASCADE,
-                onDelete = CASCADE
-        )
-})
 public class PerformanceAssessment extends Assessment{
 
-    @ColumnInfo(name = "assessment_reference")
     private int assessment_reference;
 
     public PerformanceAssessment(int assessment_id, String assessmentName, String assessmentStart, String assessmentEnd, int courseID, String type, int assessment_reference) {
